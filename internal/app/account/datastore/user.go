c55b5f4140cf6580f96cb794342f16478ad65473
Reshi E520 <reshimahendra@gmail.com>
2022-01-30 00:03:14 +0800

    complete user datastore main CRUD method

--- internal/app/account/datastore/user.go
+++ internal/app/account/datastore/user.go
@@ -1,54 +1,71 @@
 /*
-    package datastore
-    user.go
-    - implementing standar CRUD operation for user
-    TODO:
-        * Get method
-        * Gets method
-        * Update method
-        * Delete method
-        * CheckCredential for login/signin operation
-        * UserActivation method
-        * UserExist method
+   package datastore
+   user.go
+   - implementing standar CRUD operation for user
+   NOTE of method:
+       * Get method
+       * Gets method
+       * Update method
+       * Delete method
+       * CheckCredential for login/signin operation
+       * UserActivation method
+       * UserExist method
 */
 package datastore
 
 import (
 	"context"
 
-	"github.com/reshimahendra/lbw-go/internal/domain"
+	"github.com/georgysavva/scany/pgxscan"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v4"
 	"github.com/reshimahendra/lbw-go/internal/database"
+	d "github.com/reshimahendra/lbw-go/internal/domain"
+	E "github.com/reshimahendra/lbw-go/internal/pkg/errors"
 )
 
 const (
-    // prepare sql command to insert new user
-    sqlUserC = `INSERT INTO users (id,username,firstname,lastname,email,passkey,status_id,role_id,
-          created_at,updated_at) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) ON CONFLICT DO NOTHING
-          RETURNING id,username,firstname,lastname,email,passkey,user_status_id,user,role_id,
-          created_at,updated_at,activated_at`
-
+    // prepare sql command to insert new user record
+    sqlUserC = `INSERT INTO public.users (id,username,firstname,lastname,email,passkey,updated_at) 
+            VALUES ($1,$2,$3,$4,$5,$6,CURRENT_TIMESTAMP) RETURNING id,
+            username,firstname,lastname,email,user_status_id,user_role_id,created_at,updated_at`
+    sqlUserR1 = `SELECT id,username,firstname,lastname,email,user_status_id,user_role_id,
+            created_at,updated_at FROM public.users WHERE id = $1 AND deleted_at IS NULL`
+    sqlUserR = `SELECT id,username,firstname,lastname,email,user_status_id,user_role_id,
+            created_at,updated_at FROM public.users WHERE deleted_at IS NULL ORDER BY created_at`
+    sqlUserU = `UPDATE public.users SET username=$2,firstname=$3,lastname=$4,
+            email=$5,passkey=$6,user_status_id=$7,user_role_id=$8,updated_at=CURRENT_TIMESTAMP
+            WHERE id=$1 ON CONFLICT DO NOTHING RETURNING id,username,firstname,lastname,email,
+            user_status_id,user_role_id,created_at,updated_at`
+    sqlUserD = `UPDATE public.users SET updated_at=CURRENT_TIMESTAMP,deleted_at=CURRENT_TIMESTAMP 
+            WHERE id=$1 RETURNING id, username,firstname,lastname,email,user_status_id,
+            user_role_id,created_at,updated_at`
 )
 
+var (
+    // userScanAll is pgxscan.ScanAll func wrapper for user
+    scanAllFunc = pgxscan.ScanAll
+)
 // IUserStore is user interface for CRUD operation directly
 // to the database
 type IUserStore interface {
     // Create will execute sql query to insert new user.role record into the database
-    Create(input domain.User) (*domain.UserRole, error)
+    Create(input d.User) (*d.User, error)
 
     // Get will execute sql query to get user.role record from database
     // based on the given id
-    Get(id int) (*domain.User, error)
+    Get(id int) (*d.User, error)
 
     // Gets will execute sql query to get all user.role record from database
-    Gets() ([]*domain.User, error)
+    Gets() ([]*d.User, error)
 
     // Update will execute sql query to update user.role record
     // based on given input id and input data 
-    Update(id string, input domain.User) (*domain.UserRole, error)
+    Update(id string, input d.User) (*d.User, error)
 
     // Delete will do 'soft delete' instead of deleting the user.role record
     // from the database. Data should be persistant in the database
-    Delete(id int) (*domain.User, error)
+    Delete(id int) (*d.User, error)
 }
 
 // UserStore interface is instance wrapper for IDatabase interface
@@ -62,11 +79,8 @@ func NewUserStore(iDB database.IDatabase) *UserStore {
     return &UserStore{DB: iDB}
 } 
 
-// 
-func (st *UserStore) Create(input domain.User) (*domain.User, error) {
-    // add required field before insert to the input request
-    input.BeforeCreate()
-
+// Create will create new User record to database
+func (st *UserStore) Create(input d.User) (*d.User, error) {
     // execute sql command to insert new user record
     result := st.DB.QueryRow(context.Background(),sqlUserC,
         input.ID,
@@ -75,31 +89,143 @@ func (st *UserStore) Create(input domain.User) (*domain.User, error) {
         input.LastName,
         input.Email,
         input.PassKey,
-        input.StatusID,
-        input.RoleID,
-        input.CreatedAt,
-        input.UpdatedAt,
     )
 
     // prepare variable container to be used as the result query container
-    u := new(domain.User)
+    user := new(d.User)
     err := result.Scan(
-        &u.ID,
-        &u.Username,
-        &u.FirstName,
-        &u.LastName,
-        &u.Email,
-        &u.PassKey,
-        &u.StatusID,
-        &u.RoleID,
-        &u.CreatedAt,
-        &u.UpdatedAt,
-        &u.ActivatedAt,
+        &user.ID,
+        &user.Username,
+        &user.FirstName,
+        &user.LastName,
+        &user.Email,
+        &user.StatusID,
+        &user.RoleID,
+        &user.CreatedAt,
+        &user.UpdatedAt,
     )
 
-    if err != nil {
-        return nil, err
+    // check if error occur during scan
+    if err == pgx.ErrNoRows{
+        return nil, E.New(E.ErrDataIsEmpty)
+    } else if err != nil {
+        return nil, err //E.New(E.ErrDatabase)
     }
 
-    return u, nil
+    return user, nil
+}
+
+// Get will get user data from database
+func (st *UserStore) Get(id uuid.UUID) (*d.User, error) {
+    // execute sql command to retreive user record by id
+    result := st.DB.QueryRow(context.Background(), sqlUserR1, id)
+
+    // prepare to scan record data
+    user := new(d.User)    
+    err := result.Scan(
+        &user.ID,
+        &user.Username,
+        &user.FirstName,
+        &user.LastName,
+        &user.Email,
+        &user.StatusID,
+        &user.RoleID,
+        &user.CreatedAt,
+        &user.UpdatedAt,
+    )
+
+    // check if error occur during scan
+    if err == pgx.ErrNoRows{
+        return nil, E.New(E.ErrDataIsEmpty)
+    } else if err != nil {
+        return nil, err //E.New(E.ErrDatabase)
+    }
+
+    return user, nil
+}
+
+// Gets will get all user data from database
+func (st *UserStore) Gets() ([]*d.User, error) {
+    // execute sql command to retreive user record
+    results, err := st.DB.Query(context.Background(), sqlUserR)
+    if err != nil {
+        return nil, E.New(E.ErrDataIsEmpty)
+    }
+    defer results.Close()
+
+    // prepare variable for data scan container
+    usr := make([]*d.User, 0)
+    if err = scanAllFunc(&usr, results); err != nil { 
+        return nil, E.New(E.ErrDatabase)
+    }
+
+    // return user slice
+    return usr, nil
+}
+
+// Update will update user based on given id
+func (st *UserStore) Update(id string, input d.User) (*d.User, error) {
+    // execute sql command to update user record
+    result := st.DB.QueryRow(context.Background(), sqlUserU,
+        id,
+        input.Username,
+        input.FirstName,
+        input.LastName,
+        input.Email,
+        input.PassKey,
+        input.StatusID,
+        input.RoleID,
+    )
+
+    // prepare to scan record data
+    user := new(d.User)    
+    err := result.Scan(
+        &user.ID,
+        &user.Username,
+        &user.FirstName,
+        &user.LastName,
+        &user.Email,
+        &user.StatusID,
+        &user.RoleID,
+        &user.CreatedAt,
+        &user.UpdatedAt,
+    )
+
+    // check if error occur during scan
+    if err == pgx.ErrNoRows{
+        return nil, E.New(E.ErrDataIsEmpty)
+    } else if err != nil {
+        return nil, E.New(E.ErrDatabase)
+    }
+
+    return user, nil
+}
+
+// Delete will delete user record based on given id
+func (st *UserStore) Delete(id string) (*d.User, error) {
+    // execute sql command to delete user record
+    result := st.DB.QueryRow(context.Background(), sqlUserD, id)
+
+    // prepare to scan record data
+    user := new(d.User)    
+    err := result.Scan(
+        &user.ID,
+        &user.Username,
+        &user.FirstName,
+        &user.LastName,
+        &user.Email,
+        &user.StatusID,
+        &user.RoleID,
+        &user.CreatedAt,
+        &user.UpdatedAt,
+    )
+
+    // check if error occur during scan
+    if err == pgx.ErrNoRows{
+        return nil, E.New(E.ErrDataIsEmpty)
+    } else if err != nil {
+        return nil, E.New(E.ErrDatabase)
+    }
+
+    return user, nil
 }

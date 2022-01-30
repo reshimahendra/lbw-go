b1c3ae32ce9f42d3aefe07fe8d1933ce92298eb9
Reshi E520 <reshimahendra@gmail.com>
2022-01-31 00:10:56 +0800

    add convert method and fix data struct

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -51,6 +51,31 @@ type User struct {
     ActivatedAt time.Time `json:"activated_at"`
 }
 
+// IsValid is to check whether User data is valid
+func (u *User) IsValid() bool {
+    return  u.ID.String() != "" &&
+            u.Username    != "" &&
+            u.PassKey     != "" &&
+            u.FirstName   != "" &&
+            u.Email       != ""
+}
+
+// ConvertToResponse will convert User model to response dto format
+func (u *User) ConvertToResponse() *UserResponse{
+    // return UserResponse data
+    return &UserResponse{
+        ID        : u.ID,
+        Username  : u.Username,
+        FirstName : u.FirstName,
+        LastName  : u.LastName,
+        Email     : u.Email,
+        StatusID  : u.StatusID,
+        RoleID    : u.RoleID,
+        CreatedAt : u.CreatedAt,
+        UpdatedAt : u.UpdatedAt,
+    }
+}
+
 // BeforeInsert will insert required data to User
 // func (u *User) BeforeInsert() {
 //     u.ID        = uuid.New()
@@ -100,8 +125,8 @@ func (u *UserRequest) IsValid() bool {
             u.Email       != ""
 }
 
-// UserRequestToUser will convert user request dto to User
-func (u *UserRequest) UserRequestToUser() *User{
+// RequestToUser will convert user request dto to User
+func (u *UserRequest) RequestToUser() *User{
     return &User{
         ID        : u.ID,
         Username  : u.Username,
@@ -132,14 +157,43 @@ type UserResponse struct {
     Email     string        `json:"email"`
 
     // Status is status held by user
-    Status    *UserStatus   `json:"status,omitempty"`
+    StatusID  int           `json:"status_id"`
 
     // RoleID is role given to the user on the system
-    Role      *UserRole     `json:"role,omitempty"`
+    RoleID    int           `json:"role_id"`
 
     // CreatedAt is creation datetime of the record
     CreatedAt time.Time     `json:"created_at"`
 
     // UpdatedAt is the last updated datetime of the record
     UpdatedAt time.Time     `json:"updated_at"`
+}
+
+// UserCredential is struct holding user credential data
+type UserCredential struct {
+    // ID is the table primary key with uuid type
+    ID        uuid.UUID `json:"id"`
+
+    // Username is the username for the user, value must be unique
+    Username  string        `json:"username"`
+
+    // Status is status held by user
+    StatusID  int           `json:"status_id"`
+
+    // PassKey is the password for the account
+    PassKey   string    `json:"password"`
+}
+
+// IsValid is to ceck whether credential is valid
+func (u *UserCredential) IsValid() bool {
+    return u.Username != "" || u.PassKey != ""
+}
+// IsActive is to check whether user credential is active
+func (u *UserCredential) IsActive() bool {
+    return u.StatusID == 1
+}
+
+// NeedActivation is to check whether user credential is not activated yet
+func (u *UserCredential) NeedActivation() bool {
+    return u.StatusID == 0
 }

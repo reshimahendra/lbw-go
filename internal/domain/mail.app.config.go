91eba7ca7fe13b21b1970d90cd66120450629287
Reshi E520 <reshimahendra@gmail.com>
2022-02-02 14:42:21 +0800

    add new model for mail service

---
+++ internal/domain/mail.app.config.go
@@ -0,0 +1,7 @@
+/*
+   package domain
+   mail.app.config.go
+   - containing user membership mail app configuration model, request dto and response dto struct
+*/
+package domain
+
---
+++ internal/domain/mail.app.status.go
@@ -0,0 +1,24 @@
+/*
+   package domain
+   mail.app.status.go
+   - containing user mail membership status model, request dto and response dto struct
+*/
+package domain
+
+// MemberStatus hold membership_status table in database
+type MemberStatus struct {
+    // ID is user.role id. it is its primary key
+    ID          int         `json:"id"`
+
+    // StatusName is MemberStatus status name
+    StatusName    string      `json:"status_name"`
+
+    // Description is the short description of the member status
+    Description string      `json:"description,omitempty"`
+} 
+
+// IsValid is to check whether data MemberStatus record is valid
+func (m *MemberStatus) IsValid() bool {
+    return m.ID > 0 && m.StatusName != ""
+}
+
